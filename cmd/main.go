0be74d6a2cfd991e7de3946847bde2f591831fb4
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-04-22 13:08:44 -0500

    Initialized logger.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/rs/zerolog"
 	"log"
 
 	"farmers_connect/internal/config"
@@ -11,6 +12,8 @@ import (
 func main() {
 	ctx := context.Background()
 
+	logger := zerolog.Ctx(ctx)
+
 	cfg := &config.Config{}
 	if err := config.Read(cfg); err != nil {
 		log.Fatalf("Failed to read config: %v", err)
@@ -18,7 +21,7 @@ func main() {
 
 	db, err := repo.NewDB(ctx, cfg.DB)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to create db")
 	}
 	defer db.Close(ctx)
 }
