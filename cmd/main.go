0026d816c9a0958fae07ae115c0fcab82642e531
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-04-22 13:41:53 -0500

    Updated logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"context"
-	"github.com/rs/zerolog"
-	"log"
+	"os"
+	"time"
 
 	"farmers_connect/internal/config"
 	"farmers_connect/internal/repo"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	ctx := context.Background()
 
-	logger := zerolog.Ctx(ctx)
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+		NoColor:    true,
+	}).
+		With().Caller().Logger().
+		Level(zerolog.TraceLevel)
 
 	cfg := &config.Config{}
 	if err := config.Read(cfg); err != nil {
-		log.Fatalf("Failed to read config: %v", err)
+		log.Fatal().Err(err).Msg("Failed to read config")
 	}
 
 	db, err := repo.NewDB(ctx, cfg.DB)
 	if err != nil {
-		logger.Fatal().Err(err).Msg("Failed to create db")
+		log.Fatal().Err(err).Msg("Failed to connect to db")
 	}
 	defer db.Close(ctx)
 }
