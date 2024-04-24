05067b043f682ad72d4f85313d007905a76f2445
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-04-23 22:04:25 -0500

    Updated farmer db struct

--- internal/repo/farmer.go
+++ internal/repo/farmer.go
@@ -1,14 +1,19 @@
 package repo
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 type FarmersRepo interface {
 }
 
 type Farmer struct {
 	ID          string    `db:"id"`
-	TelegramID  *int64    `db:"telegram_id"`
-	NickName    *string   `db:"nickname"`
+	UserID      string    `db:"user_id"`
+	FullName    *string   `db:"full_name"`
+	CompanyName *string   `db:"company_name"`
 	ProfilePic  *string   `db:"profile_pic"`
 	Description *string   `db:"description"`
 	Products    []Product `db:"products"`
@@ -16,4 +21,23 @@ type Farmer struct {
 	Reviews     []Review  `db:"reviews"`
 	Location    string    `db:"location"`
 	CreatedAt   time.Time `db:"created_at"`
+}
+
+type farmersRepo struct {
+	db DB
+}
+
+func NewFarmersRepo(db DB) *farmersRepo {
+	return &farmersRepo{db: db}
+}
+
+func (f *farmersRepo) Create(ctx context.Context, userID, fullName string) (*Farmer, error) {
+	farmer := &Farmer{}
+	err := f.db.GetContext(ctx, farmer, `
+		insert into farmers (user_id, full_name) values ($1, $2) returning *
+	`, userID, fullName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to exec: %w", err)
+	}
+	return farmer, nil
 }
