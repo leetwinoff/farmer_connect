8d0e827258b016e96fbbda3e894d4a6a9dc32a05
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-04-23 21:04:17 -0500

    Updated product db struct

--- internal/repo/product.go
+++ internal/repo/product.go
@@ -1,4 +1,18 @@
 package repo
 
+import "time"
+
+type ProductRepo interface {
+}
+
 type Product struct {
+	ID          string    `db:"id"`
+	Name        string    `db:"name"`
+	Type        string    `db:"type"`
+	ProductPic  *string   `db:"product_pic"`
+	Description *string   `db:"description"`
+	Price       int       `db:"price"`
+	Unit        string    `db:"unit"`
+	Quantity    int       `db:"quantity"`
+	CreatedAt   time.Time `db:"created_at"`
 }
