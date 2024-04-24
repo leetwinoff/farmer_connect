411ba3a75af7229cea87b26923059b22f2131485
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-04-23 21:03:31 -0500

    Updated consumer db struct

--- internal/repo/consumer.go
+++ internal/repo/consumer.go
@@ -1,1 +1,16 @@
 package repo
+
+type ConsumerRepo interface {
+}
+
+type Consumer struct {
+	ID            string    `db:"id"`
+	TelegramID    *int64    `db:"telegram_id"`
+	NickName      *string   `db:"nickname"`
+	ProfilePic    *string   `db:"profile_pic"`
+	Description   *string   `db:"description"`
+	Location      string    `db:"location"`
+	SavedFarmers  []Farmer  `db:"saved_farmers"`
+	Reviews       []Review  `db:"reviews"`
+	SavedProducts []Product `db:"saved_products"`
+}
